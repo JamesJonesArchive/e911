8aa27d86454955d79a82dcb1a8028385a9f67180
James Jones <jamjon3@gmail.com>
2015-05-19 15:36:44 -0400

    Added angular support and some basic stub routing

---
+++ js/app.js
@@ -0,0 +1,21 @@
+(function (window, angular, undefined) {
+    'use strict';
+
+    angular
+    .module('e911App', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/e911.html',
+                controller: 'e911Ctrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
+})(window, window.angular);
---
+++ js/controllers/e911.js
@@ -0,0 +1,10 @@
+(function (window, angular, undefined) {
+    'use strict';
+    angular
+    .module('e911App')
+    .controller('e911Ctrl', ['$scope', function ($scope) { 
+  
+    }]);
+    
+})(window, window.angular);
+
