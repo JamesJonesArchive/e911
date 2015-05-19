3c29656f2ea6759bdbbbe0cbb29dc5c1cc44e6b7
James Jones <jamjon3@gmail.com>
2015-05-19 16:15:53 -0400

    little more experimenting

--- js/controllers/e911.js
+++ js/controllers/e911.js
@@ -3,7 +3,15 @@
     angular
     .module('e911App')
     .controller('e911Ctrl', ['$scope', function ($scope) { 
-  
+        $scope.acknowlegements = [];
+        $scope.acknowledge = function(type) {
+            if(!(type in $scope.acknowlegements)) {
+                $scope.acknowlegements.push(type);
+            }
+        };
+        $scope.isAcknowledged = function(type) {
+            return (type in $scope.acknowlegements);
+        };
     }]);
     
 })(window, window.angular);
