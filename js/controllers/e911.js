c60d8f8bfd067868fc907697c3857b323a2668c0
James Jones <jamjon3@gmail.com>
2015-05-22 15:11:03 -0400

    More wiring

--- js/controllers/e911.js
+++ js/controllers/e911.js
@@ -2,7 +2,7 @@
     'use strict';
     angular
     .module('e911App')
-    .controller('e911Ctrl', ['$scope','$routeParams', function ($scope,$routeParams) { 
+    .controller('e911Ctrl', ['$scope','$routeParams','e911Service', function ($scope,$routeParams,e911Service) { 
         if('id' in $routeParams) {
             $scope.id = $routeParams.id;
         }
@@ -16,7 +16,16 @@
             return (type in $scope.acknowlegements);
         };
         $scope.e911sign = function() {
-            alert($scope.id);
+            // alert($scope.id);
+            e911Service.e911sign($scope.id).
+            success(function(data, status, headers, config) {
+              // this callback will be called asynchronously
+              // when the response is available
+            }).
+            error(function(data, status, headers, config) {
+              // called asynchronously if an error occurs
+              // or server returns response with an error status.
+            });
         };
     }]);
     
---
+++ js/services/e911.js
@@ -0,0 +1,13 @@
+(function (window, angular, undefined) {
+    'use strict';
+
+    angular.module('e911App')
+        .factory('e911Service', ['$http', function ($http) {
+            var service = {
+                e911sign: function (id) {
+                    return $http.post('/src/e911sign', {id: id});
+                }
+            };
+            return service;
+        }]);
+})(window, window.angular);
