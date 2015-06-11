c05a75d704e565491c501d7025d1f6fccd232f50
James Jones <jamjon3@gmail.com>
2015-06-11 12:24:40 -0400

    Adding license agreement

--- app/api/index.php
+++ app/api/index.php
@@ -1,11 +1,18 @@
 <?php
-//ini_set('display_errors',1);
-//ini_set('display_startup_errors',1);
-//error_reporting(-1);
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2015 University of South Florida
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 try {
 // Initialize Composer autoloader
@@ -29,9 +36,7 @@ try {
             $app->response->headers->set('Content-Type', 'application/json');
             $app->response->setBody($resp->encode());
         } else {
-//            $resp = $e911services->e911sign('EDSfMJgt5E8DJIebTJnynWcrGktiHkFD/Xp0ZjyPP9/Dk6JkvoIvdP57J8TMgH115Dd7PM2+mPBvGgw1sKP2lA==');
             $app->response->headers->set('Content-Type', 'application/json');
-//            $app->response->setBody($resp->encode());
             $app->response->setBody((new \JSend\JSendResponse('fail', [
                 'e911sign' => false
             ]))->encode());
--- app/scripts/controllers/e911.js
+++ app/scripts/controllers/e911.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2015 University of South Florida
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 (function (window, angular, undefined) {
     'use strict';
     angular
@@ -35,16 +51,6 @@
                 alert("Error! E911 Acknowledgement could not be updated"+JSON.stringify(response));
                 $window.parent.postMessage(JSON.stringify($scope.e911), "*");
             });
-//            e911Service.e911sign($scope.id).
-//            success(function(data, status, headers, config) { 
-//                $scope.e911 = { e911: data };
-//                $window.parent.postMessage(JSON.stringify($scope.e911), "*");
-//            }).
-//            error(function(data, status, headers, config) {
-//                $scope.e911 = { e911: data };
-//                alert("Error! E911 Acknowledgement could not be updated");
-//                $window.parent.postMessage(JSON.stringify($scope.e911), "*");
-//            });
         };
         $scope.acknowledgeClasses = function(type) {
             switch(type) {
